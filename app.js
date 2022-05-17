5b83d4c9f1984fd124a5afdd810eac41040911c6
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-17 20:51:48 +0300

    feat: add min server stream pipe zlib

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
+require("http").createServer((q,s)=>s.end('Hi')).listen(10);
 
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
+const fs = require("fs");
+ 
+// let writeableStream = fs.createWriteStream("hello.txt");
+// writeableStream.write("Привет мир!\n");
+// writeableStream.write("Продолжение записи \n");
+// writeableStream.end("Завершение записи");
+// let readableStream = fs.createReadStream("hello.txt", "utf8");
+ 
+// readableStream.on("data", chunk=>{ 
+//     console.log(chunk);
+// });
+ 
 
-// node app.js Tom 23
+const zlib = require("zlib");
+  
+let readableStream = fs.createReadStream("hello.txt", "utf8");
+  
+let writeableStream = fs.createWriteStream("hello.txt.gz");
+  
+let gzip = zlib.createGzip();
+  
+readableStream.pipe(gzip).pipe(writeableStream);
